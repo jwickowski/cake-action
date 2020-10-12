28ab87d768207bc5b6ce781d9391dd293c538cc5
Jerzy Wickowski <jerzy.wickowski@ajkvision.eu>
2020-10-12 14:00:57 +0200

    1111111

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { ToolsDirectory } from "./toolsDirectory";
 import * as dotnet from "./dotnet";
 import * as cake from "./cake";
 import { CakeArgument } from "./cakeParameter";
+import yargsParser from "yargs-parser";
 
 export async function run() {
   try {
@@ -12,7 +13,7 @@ export async function run() {
       (core.getInput("cake-bootstrap") || "").toLowerCase() === "true";
     const target = new CakeArgument("target", core.getInput("target"));
     const verbosity = new CakeArgument("verbosity", core.getInput("verbosity"));
-    const scriptArgs = new CakeArgument("assemblyVersion", "1.2.3");
+    const scriptArgs = getScriptArguments();
 
     const toolsDir = new ToolsDirectory();
     toolsDir.create();
@@ -31,11 +32,32 @@ export async function run() {
       toolsDir,
       target,
       verbosity,
-      scriptArgs
+      ...scriptArgs
     );
   } catch (error) {
     core.setFailed(error.message);
   }
+}
+
+function getScriptArguments(): CakeArgument[] {
+  const result: CakeArgument[] = [];
+  const input: string = core.getInput("script-arguments");
+  if (!input) {
+    return [];
+  }
+
+  const options = yargsParser(input);
+  const optionsKeys: string[] = Object.keys(options);
+  optionsKeys.forEach((optionsKey) => {
+    // we ignore that because of yargs-parser implementation
+    if (optionsKey === "_") {
+      return;
+    }
+
+    const cakeArgument = new CakeArgument(optionsKey, options[optionsKey]);
+    result.push(cakeArgument);
+  });
+  return result;
 }
 
 run();
