ba2317ee21abf8faa46b14eb964e0d93e898712a
Jerzy Wickowski <jerzy.wickowski@ajkvision.eu>
2020-10-12 13:51:52 +0200

    hordcode

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ export async function run() {
       (core.getInput("cake-bootstrap") || "").toLowerCase() === "true";
     const target = new CakeArgument("target", core.getInput("target"));
     const verbosity = new CakeArgument("verbosity", core.getInput("verbosity"));
-    const scriptArgs = new CakeArgument("ScriptArgs", core.getInput("script-arguments"));
+    const scriptArgs = new CakeArgument("assemblyVersion", "1.2.3");
 
     const toolsDir = new ToolsDirectory();
     toolsDir.create();
